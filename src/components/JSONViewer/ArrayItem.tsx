c59a82c31e4e0e5a87c7ef562805b50a0ee3648e
thangtm2 <thangtm2@vng.com.vn>
2023-11-17 10:29:17 +0700

    feat: support collapsed for JSONViewer

--- src/components/JSONViewer/ArrayItem.tsx
+++ src/components/JSONViewer/ArrayItem.tsx
@@ -1,4 +1,4 @@
-import { Children } from "react";
+import { Children, useState } from "react";
 import { ItemProps } from "./types";
 import FieldName from "./FieldName";
 
@@ -7,19 +7,71 @@ export default function ArrayItem({
   name,
   children,
 }: ItemProps<Array<DataType>>) {
+  const [collapsed, setCollapsed] = useState(false);
+  if (!collapsed) {
+    return (
+      <div className="w-full" id={name}>
+        <FieldName value={fieldName} />
+        <span className="text-base font-semibold text-teal-500">
+          {"["}
+          <button
+            className="ml-1 inline-flex h-5 w-5 items-center justify-center rounded-sm border-2 border-solid border-base-300 align-bottom text-base-content"
+            onClick={() => setCollapsed(true)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+            </svg>
+          </button>
+          {renderNumberOfItems(Children.toArray(children).length)}
+        </span>
+        <div className="border-l-2 border-dashed border-base-300 pl-6">
+          {children}
+        </div>
+        <span className="text-base font-semibold text-teal-500">{"]"}</span>
+      </div>
+    );
+  }
   return (
     <div className="w-full" id={name}>
       <FieldName value={fieldName} />
-      <span className="ml-1 text-base	font-semibold">
-        {"["}
-        <span className="ml-1 text-sm text-neutral-content">
-          {Children.toArray(children).length} items
-        </span>
+      <span className="text-base font-semibold text-teal-500">
+        <span onClick={() => setCollapsed(false)}>{"[...]"}</span>
+        <button
+          className="ml-1 inline-flex h-5 w-5 items-center justify-center rounded-sm border-2 border-solid border-base-300 align-bottom text-base-content"
+          onClick={() => setCollapsed(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v12m6-6H6"
+            />
+          </svg>
+        </button>
+        {renderNumberOfItems(Children.toArray(children).length)}
       </span>
-      <div className="border-l-2 border-dashed border-base-300 pl-6">
-        {children}
-      </div>
-      <span className="text-base font-semibold">{"]"}</span>
     </div>
+  );
+}
+
+function renderNumberOfItems(value: number) {
+  return (
+    <span className="ml-1 bg-base-200 p-1 text-sm text-base-content">
+      {value} items
+    </span>
   );
 }
--- src/components/JSONViewer/ObjectItem.tsx
+++ src/components/JSONViewer/ObjectItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FieldName from "./FieldName";
 import { ItemProps } from "./types";
 
@@ -6,14 +7,61 @@ export default function ObjectItem({
   name,
   children,
 }: ItemProps<object>) {
+  const [collapsed, setCollapsed] = useState(false);
+  if (!collapsed) {
+    return (
+      <div id={name}>
+        <FieldName value={fieldName} />
+        <span className="text-base font-semibold">
+          {"{"}
+          <button
+            className="ml-1 inline-flex h-5 w-5 items-center justify-center rounded-sm border-2 border-solid border-base-300 align-bottom text-base-content"
+            onClick={() => setCollapsed(true)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+            </svg>
+          </button>
+        </span>
+        <div className="border-l-2 border-dashed border-base-300 pl-6">
+          {children}
+        </div>
+        <span className="text-base font-semibold">{"}"}</span>
+      </div>
+    );
+  }
   return (
     <div id={name}>
       <FieldName value={fieldName} />
-      <span className="pl-1	text-base	font-semibold">{"{"}</span>
-      <div className="border-l-2 border-dashed border-base-300 pl-6">
-        {children}
-      </div>
-      <span className="text-base font-semibold">{"}"}</span>
+      <span className="text-base font-semibold">
+        {"{...}"}
+        <button
+          className="ml-1 inline-flex h-5 w-5 items-center justify-center rounded-sm border-2 border-solid border-base-300 align-bottom text-base-content"
+          onClick={() => setCollapsed(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v12m6-6H6"
+            />
+          </svg>
+        </button>
+      </span>
     </div>
   );
 }
